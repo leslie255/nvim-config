bbf67fbfdc3099818614e62fb099c971b9df564d
Ptrzl <zl2005.luo@icloud.com>
2022-02-13 01:11:07 +0800

    Clean up more code

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -2,14 +2,14 @@ local M = {}
 function M.config()
     -- nvim-treesitter config
     require'nvim-treesitter.configs'.setup {
-      -- ensure_installed = "maintained", -- for installing all maintained parsers
-      ensure_installed = { "c", "cpp", "rust", "lua" }, -- for installing specific parsers
-      sync_install = true, -- install synchronously
-      ignore_install = { }, -- parsers to not install
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false, -- disable standard vim highlighting
-      },
+        -- ensure_installed = "maintained", -- for installing all maintained parsers
+        ensure_installed = { "c", "cpp", "rust", "lua" }, -- for installing specific parsers
+        sync_install = true, -- install synchronously
+        ignore_install = { }, -- parsers to not install
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false, -- disable standard vim highlighting
+        },
     }
 end
 return M
