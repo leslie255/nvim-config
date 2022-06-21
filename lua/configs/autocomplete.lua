2ce14f18cfa95c205b6b760d3ca6fe64cce40e20
Leslie Kerman <kerman255@icloud.com>
2022-06-22 00:18:39 +0800

    Bugfix: autocomplete cannot be selected properly

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -61,6 +61,46 @@ function M.config()
         })
     })
 
+    local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+    
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
+    
+    cmp.setup({
+    
+      mapping = {
+    
+        ["<C-n>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+    
+        ["<C-p>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+    
+        -- ... Your other mappings ...
+      },
+    
+      -- ... Your other configuration ...
+    })
+
     -- nvim-lspconfig config
     -- List of all pre-configured LSP servers:
     -- github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
