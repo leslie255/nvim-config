d96785057c687574dce80883dd964e9331f50651
Leslie Kerman <kerman255@icloud.com>
2022-08-25 17:34:35 +0800

    Update bufferline icon config for newer version

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -12,7 +12,10 @@ function M.config()
 			-- NOTE: this plugin is designed with this icon in mind,
 			-- and so changing this is NOT recommended, this is intended
 			-- as an escape hatch for people who cannot bear it for whatever reason
-			indicator_icon = '▎',
+			indicator = {
+				icon = '▎',
+				style = 'icon'
+			},
 			buffer_close_icon = '',
 			modified_icon = '●',
 			close_icon = '',
